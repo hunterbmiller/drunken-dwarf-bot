8fc13edb7a1e166bd813f38699024d54ef5ad1c6
Hunter Miller <hello@huntermiller.xyz>
2021-10-02 20:47:26 -0600

    adds working initial weather command

--- constants.js
+++ constants.js
@@ -2,3 +2,4 @@ module.exports.COMMAND_PREFIX = '!';
 module.exports.PING_COMMAND = 'ping';
 module.exports.F_TO_C_COMMAND = 'f_to_c';
 module.exports.C_TO_F_COMMAND = 'c_to_f';
+module.exports.WEATHER_COMMAND = 'weather';
